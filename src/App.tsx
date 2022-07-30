aa1c9f6d15c0cd9bbedf6d192cd7ea18fe296518
hwajingh <51863703+hwajingh@users.noreply.github.com>
2022-07-30 18:43:59 -0400

    initialise

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styles from "./app.module.css";
+import "./App.css";
+import IconButton from "@mui/material/IconButton";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+import Checkbox from "@mui/material/Checkbox";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function App() {
+  const ogList = localStorage.getItem("list");
+  const ogCheck = localStorage.getItem("check");
+  const [toDoList, setToDoList] = useState<
+    { item: string; checked: boolean }[]
+  >(ogList ? JSON.parse(ogList) : []);
+  const [item, setItem] = useState<string>("");
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    const list = toDoList;
+    list[index] = { item: list[index].item, checked: !list[index].checked };
+    localStorage.setItem("list", JSON.stringify(toDoList));
+    setToDoList([...list]);
+  };
+
+  const addToList = () => {
+    const list = toDoList;
+    list.push({ item, checked: false });
+    setToDoList([...list]);
+    setItem("");
+    localStorage.setItem("list", JSON.stringify(toDoList));
+  };
+
+  const deleteFromList = (index: number) => {
+    const list = toDoList;
+    list.splice(index, 1);
+    setToDoList([...list]);
+    localStorage.setItem("list", JSON.stringify(toDoList));
+  };
+
+  toDoList.sort((a, b) => {
+    return a.checked ? 1 : -1;
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={styles.root}>
+        <h1 className={styles.header}>To Do List </h1>
+        <div className={styles.head}>
+          <TextField
+            id="filled-basic"
+            label=""
+            variant="filled"
+            placeholder="Add something"
+            onChange={(e) => setItem(e.target.value)}
+            value={item}
+            size="small"
+            fullWidth
+          />
+          <IconButton disabled={item.replace(/\s/g, "") === ""} size="large">
+            <AddIcon
+              onClick={() => {
+                addToList();
+              }}
+            />
+          </IconButton>
+        </div>
+
+        <ul className={styles.list}>
+          {toDoList.map((item, index) => (
+            <div className={styles.individualItem}>
+              <Checkbox
+                className={styles.checkbox}
+                checked={item.checked}
+                onChange={(e) => handleChange(e, index)}
+              />
+              <li className={item.checked ? styles.strike : styles.overflow}>
+                {item.item}
+              </li>
+              <IconButton className={styles.delete}>
+                <DeleteIcon
+                  onClick={() => {
+                    deleteFromList(index);
+                  }}
+                />
+              </IconButton>
+            </div>
+          ))}
+        </ul>
+        <div className={styles.footer}>Made by Tiger Hong</div>
+      </div>
     </div>
   );
 }
